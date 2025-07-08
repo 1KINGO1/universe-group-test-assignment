be6cda5e78701acee1a66f2d66a5bc65df2d9824
KINGO <spental12345@gmail.com>
2025-07-08 19:12:08 +0300

    Added reporter & user model indexes

---
+++ shared/prisma/migrations/20250708160940_added_indexes_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "users_gender_idx" ON "users"("gender");
+
+-- CreateIndex
+CREATE INDEX "users_country_idx" ON "users"("country");
+
+-- CreateIndex
+CREATE INDEX "users_city_idx" ON "users"("city");
+
+-- CreateIndex
+CREATE INDEX "users_age_idx" ON "users"("age");
