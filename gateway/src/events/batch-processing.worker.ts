0b862406b71e7af94bd6558c4bbc05d7cbcb0064
KINGO <spental12345@gmail.com>
2025-07-11 12:43:28 +0300

    Added basic unit tests for gateway & gateway-processor

--- gateway/src/events/batch-processing.worker.ts
+++ gateway/src/events/batch-processing.worker.ts
@@ -1,21 +1,8 @@
 import { parentPort } from 'worker_threads';
-import { Prisma } from '@prisma/client';
-import type { Event } from "@kingo1/universe-assignment-shared";
-
-interface ProcessResult {
-	outboxEvents: Prisma.OutboxEventCreateManyInput[];
-}
+import { processEvents } from './utils/process-events';
+import { Event } from '@kingo1/universe-assignment-shared';
 
 parentPort!.on('message', (events: Event[]) => {
-	const outboxEvents: ProcessResult['outboxEvents'] = [];
-
-	for (const event of events) {
-		const base: Omit<Prisma.OutboxEventCreateManyInput, 'status' | 'error'> = {
-			payload: JSON.stringify(event),
-		};
-
-		outboxEvents.push(base);
-	}
-
-	parentPort!.postMessage({ outboxEvents } as ProcessResult);
+	const result = processEvents(events);
+	parentPort!.postMessage(result);
 });
---
+++ gateway/src/events/utils/process-events.ts
@@ -0,0 +1,20 @@
+import { Prisma } from '@prisma/client';
+import type { Event } from "@kingo1/universe-assignment-shared";
+
+export interface ProcessResult {
+	outboxEvents: Prisma.OutboxEventCreateManyInput[];
+}
+
+export function processEvents(events: Event[]): ProcessResult {
+	const outboxEvents: ProcessResult['outboxEvents'] = [];
+
+	for (const event of events) {
+		const base: Omit<Prisma.OutboxEventCreateManyInput, 'status' | 'error'> = {
+			payload: JSON.stringify(event),
+		};
+
+		outboxEvents.push(base);
+	}
+
+	return { outboxEvents };
+}
